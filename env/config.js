2e63f29032eb1fde10bf09e8715971d5466cdd93
njabulo.thwala <njabulo.thwala@symbiotics.co.za>
2017-08-31 19:02:08 +0200

    "Added environment config file"

---
+++ env/config.js
@@ -0,0 +1,23 @@
+var config = {
+    development: {
+        //Development database url
+        dburl: 'mongodb://localhost/suggestionBoxDB',
+        
+        //server details
+        server: {
+            host: '127.0.0.1',
+            port: '3000'
+        }
+    },
+    production:{
+        //Production database url
+        dburl: 'mongodb://njabulothwala23:TomasCook23%3F@cluster0-shard-00-00-kpwka.mongodb.net:27017,cluster0-shard-00-01-kpwka.mongodb.net:27017,cluster0-shard-00-02-kpwka.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+
+        //yet to configure aws settings
+        server: {
+            host: '127.0.0.1',
+            port: '3000'
+        }
+    }
+    };
+    module.exports = config;
