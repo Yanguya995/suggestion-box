db3f290860dbe00fd1d20e774570d3a5c5ae0bc3
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2017-12-13 04:59:12 +0200

    Population and respnses are functioning correctly

--- env/config.js
+++ env/config.js
@@ -10,17 +10,12 @@ var config = {
         }
     },
     production:{
-        //Production database url
-        dburl: 'mongodb://njabulothwala23:TomasCook23%3F@cluster0-shard-00-00-kpwka.mongodb.net:27017,cluster0-shard-00-01-kpwka.mongodb.net:27017,cluster0-shard-00-02-kpwka.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
-
-        //yet to configure aws settings
+        dburl: 'mongodb://njabulothwala23:njabulothwala23@cluster0-shard-00-00-ij80q.mongodb.net:27017,cluster0-shard-00-01-ij80q.mongodb.net:27017,cluster0-shard-00-02-ij80q.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
         server: {
-            host: '127.0.0.1',
+            host: 'http://ec2-34-235-230-124.compute-1.amazonaws.com',
             port: '3000'
         },
-
-        secret: '123Secret?456?anothersecret??**how_Have_=_uBeen_BoBBo..<<>>:""QWE nm.,nmdgdfgdfgd'
-
+        secret: '123Secret?456?anothersecret??**how_Have_=_uBeen_BoBBo..<<kjsdh5262728292010k21sdfsdf2s22324252627sdfsdfsdf28292010sdjf>>:""QWE nm.,nmdgdfgdfgd'
     }
     };
     module.exports = config;
